bf72ca8b3338153f55f7d9e27fa897d3dd8664ec
dexterlautenbach <dexter@thewateringcan.ca>
2022-04-21 09:10:56 -0400

    Paid V1.021 working api with one time fire

--- src/components/checkout/endTransaction/endtransaction.jsx
+++ src/components/checkout/endTransaction/endtransaction.jsx
@@ -3,28 +3,38 @@ import React, {Component} from "react";
 class EndTransaction extends Component {
     state = {};
 
+    /** when this compoentent is fired, first thing it needs to do is create a basic order inside of woo so that we may get the order number
+     * the order number is handed back to the main checkout component.
+     * only when user selects "finish sale" does all the order information get pushed through
+     */
+
     componentDidMount() {
+        (this.props.orderID > 0)? console.log(this.props.orderID) : this.createWooOrder();
+
+
+    }
+
+    createWooOrder ()  {
         // POST request using fetch with set headers
         const requestOptions = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(this.getOrderData())
+            body: JSON.stringify(this.getOrderDataBasic())
         };
+
         fetch('https://test.wateringcanworkshops.com/wp-json/wc/v3/orders?consumer_key=ck_181949d267953fb08db6575c5aefcc09b6592080&consumer_secret=cs_4c327415a7ad72aafd384f806ca35b3a932d35f7', requestOptions)
             .then(response => response.json())
             .then((data) => {
-            this.setState({order: data});
-            this.props.setorderID(data.id);
-        });
-
-
+                this.setState({order: data});
+                this.props.setorderID(data.id);
+            });
     }
 
 
 
-    getOrderData = () => {
+    getOrderDataBasic = () => {
         const lineItems = this.getLineItems();
         return {
             payment_method : 'cash',
