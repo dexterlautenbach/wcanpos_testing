7d79645f093e522f2294c53a0330c4dcff25806c
dexterlautenbach <dexter@thewateringcan.ca>
2022-04-25 14:06:30 -0400

    V1.0601 working stripe connection token broken terminal

--- src/components/checkout/paymentMethods/stripeTerminal/stripeBody.jsx
+++ src/components/checkout/paymentMethods/stripeTerminal/stripeBody.jsx
@@ -1,22 +1,52 @@
 import React, {Component} from "react";
+import '@stripe/react-stripe-js';
+import '@stripe/stripe-js';
 import '@stripe/terminal-js';
+import {loadStripeTerminal} from '@stripe/terminal-js';
 
 
 
 class StripeBody extends Component {
     state = {};
 
-   async componentDidMount() {
+    fetchConnectionToken = () => {
         const apiSearch = "https://test.wateringcanworkshops.com/wp-json/pos_bridge/v1/stripe_connection_token?";
         const consumerKey = 'consumerKey=U59ws06BB0B00A2gL2saOx92o44w68R6ti1o26aquDYcT65b4728vcfYN7xA7XIifHenpr8qG6V0Cw76kJp7xsbeiHSdGUjxB2hUts74RGjBM3AHgm1HYb1xC4yg6k8yqo8nQN4QJa8aYvii1T0ot0VQ6nyDe0KARlvIv03Z84wO369LrY9V8Bm6v5L9N9fax0hJvj45';
         const secret = 'secret=VTE5eXq2zim496P6a82Y31x5xIUaI4reWI6dlKC5KZkDX7J1h3isK518yG6Ntngtt58lQcnIRxain39uK776pJ7QXR60600PX92RgmcSrFJ2s9getmfdB4mX4jo1HLjt850cyL139Q1eCBk3ZB5ZU5osmMjD6Ucl9mS0vjAilcf01p18f78aXM1oUa283dvkkf5Vi3c3';
 
         const url = apiSearch + consumerKey + "&" + secret;
-        const response = await fetch(url);
-        const data = await response.json();
+        const response =  fetch(url);
+        const data =  response.json();
         //console.log(data);
 
         console.log(data.secret);
+        return data.secret;
+    }
+
+    createTerminalConnection = () =>{
+        const StripeTerminal = loadStripeTerminal();
+        const terminal = StripeTerminal.create({
+            onFetchConnectionToken: async ()=>{
+                const apiSearch = "https://test.wateringcanworkshops.com/wp-json/pos_bridge/v1/stripe_connection_token?";
+                const consumerKey = 'consumerKey=U59ws06BB0B00A2gL2saOx92o44w68R6ti1o26aquDYcT65b4728vcfYN7xA7XIifHenpr8qG6V0Cw76kJp7xsbeiHSdGUjxB2hUts74RGjBM3AHgm1HYb1xC4yg6k8yqo8nQN4QJa8aYvii1T0ot0VQ6nyDe0KARlvIv03Z84wO369LrY9V8Bm6v5L9N9fax0hJvj45';
+                const secret = 'secret=VTE5eXq2zim496P6a82Y31x5xIUaI4reWI6dlKC5KZkDX7J1h3isK518yG6Ntngtt58lQcnIRxain39uK776pJ7QXR60600PX92RgmcSrFJ2s9getmfdB4mX4jo1HLjt850cyL139Q1eCBk3ZB5ZU5osmMjD6Ucl9mS0vjAilcf01p18f78aXM1oUa283dvkkf5Vi3c3';
+
+                const url = apiSearch + consumerKey + "&" + secret;
+                const response =  fetch(url);
+                const data =  response.json();
+                //console.log(data);
+
+                console.log(data.secret);
+                return data.secret;
+            },
+            onUnexpectedReaderDisconnect: this.unexpectedDiscounnect,
+        });
+
+        return terminal;
+    }
+
+    unexpectedDiscounnect = () =>{
+        console.log('well fuck');
     }
 
 
@@ -24,7 +54,7 @@ class StripeBody extends Component {
         return (
             <div className="modal-body">
                 hi
-
+                {this.createTerminalConnection()}
             </div>
         )
     }
